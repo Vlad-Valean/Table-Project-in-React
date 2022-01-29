fb63a9080679af4492849a72418667eb659e5cd2
Vlad Valean <vlad.valean19@gmail.com>
2022-01-29 19:44:22 +0200

    Banner component is done more to come

---
+++ frontend/src/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Banner() {
+    return (
+        <header className='banner'>
+            <h1>Shopping & User Management App</h1>
+        </header>
+    );
+}
+
+export default Banner;
